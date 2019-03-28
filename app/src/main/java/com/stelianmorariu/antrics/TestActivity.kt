ffa6f319b25e9417fcd11f37485c9fc326197405
stelian <stelian.morariu@gmail.com>
2019-03-28 09:52:39 +0000

    add initial  collapsing toolbar with MotionLayout

---
+++ app/src/main/java/com/stelianmorariu/antrics/TestActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+package com.stelianmorariu.antrics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
