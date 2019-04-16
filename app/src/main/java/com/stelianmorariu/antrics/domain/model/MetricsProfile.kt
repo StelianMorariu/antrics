2a4a08552b289ca14268dbf388bd12a6c6f33d45
stelian <stelian.morariu@gmail.com>
2019-04-16 17:23:57 +0100

    make MetricsProfile parcelable

--- app/src/main/java/com/stelianmorariu/antrics/domain/model/MetricsProfile.kt
+++ app/src/main/java/com/stelianmorariu/antrics/domain/model/MetricsProfile.kt
@@ -4,20 +4,26 @@
 
 package com.stelianmorariu.antrics.domain.model
 
-data class MetricsProfile(val deviceCode:String,
-                          val deviceName: String,
-                          val densityQualifier:Float,
-                          val densityDpi: Int, // ex: 560 ddpi
-                          val densityBucket:String, // ex: xxhdpi
-                          val aspectRatio: Float,
-                          val format:String, // ex: long
-                          val widthPx:Int,
-                          val heightPx:Int,
-                          val widthDp:Int,
-                          val heightDp:Int)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MetricsProfile(
+    val deviceCode: String,
+    val deviceName: String,
+    val densityQualifier: Float,
+    val densityDpi: Int, // ex: 560 ddpi
+    val densityBucket: String, // ex: xxhdpi
+    val aspectRatio: Float,
+    val format: String, // ex: long
+    val widthPx: Int,
+    val heightPx: Int,
+    val widthDp: Int,
+    val heightDp: Int
+) : Parcelable
 
 
-inline fun emptyMetricsProfile(): MetricsProfile {
+fun emptyMetricsProfile(): MetricsProfile {
     return MetricsProfile(
         "",
         "",
