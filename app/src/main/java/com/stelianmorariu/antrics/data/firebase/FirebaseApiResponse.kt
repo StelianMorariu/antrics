563bd9903861612298f50d2e158424bc2e7caa92
stelian <stelian.morariu@gmail.com>
2019-06-12 17:40:11 +0100

    create initial firebase data source

---
+++ app/src/main/java/com/stelianmorariu/antrics/data/firebase/FirebaseApiResponse.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+package com.stelianmorariu.antrics.data.firebase
+
+data class FirebaseDeviceMetadata(
+    val model: String,
+    val marketingName: String
+)
---
+++ app/src/main/java/com/stelianmorariu/antrics/data/firebase/FirebaseDataSource.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+package com.stelianmorariu.antrics.data.firebase
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.stelianmorariu.antrics.domain.model.MetricsProfile
+import com.stelianmorariu.antrics.domain.model.StatefulResource
+
+
+class FirebaseDataSource {
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+
+    fun getDeviceMetaData(deviceModel: String): LiveData<StatefulResource<FirebaseDeviceMetadata>> {
+        val result: MutableLiveData<StatefulResource<FirebaseDeviceMetadata>> = MutableLiveData()
+
+        result.postValue(StatefulResource.loading(null))
+
+        val deviceMetaDataQuery = firestore
+            .collection(DEVICES_COLLECTION_NAME)
+            .whereEqualTo(DEVICE_MODEL_KEY, deviceModel)
+
+        deviceMetaDataQuery.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            if (firebaseFirestoreException != null) {
+                result.postValue(StatefulResource.error(firebaseFirestoreException.toString(), null))
+            }
+
+            querySnapshot?.let { snapshot ->
+                if (snapshot.isEmpty) {
+                    result.postValue(StatefulResource.error("No result found for $deviceModel", null))
+                } else {
+                    val metadata = snapshot.documents.first().toObject(FirebaseDeviceMetadata::class.java)
+                    result.postValue(StatefulResource.success(metadata))
+                }
+            }
+        }
+
+        return result
+    }
+
+    fun saveDeviceMetrics(deviceMetricsPprofile: MetricsProfile) {
+
+    }
+
+    companion object {
+        private const val DEVICES_COLLECTION_NAME = "devices"
+        private const val DEVICE_IMAGES_COLLECTION_NAME = ""
+
+        private const val DEVICE_MODEL_KEY = "device_model"
+
+    }
+}
