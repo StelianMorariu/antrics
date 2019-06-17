3fde5aa37b22428db10570fc3d5f46b397778728
stelian <stelian.morariu@gmail.com>
2019-06-17 11:40:32 +0100

    add Firebase anonymous auth to access firestore

--- app/src/main/java/com/stelianmorariu/antrics/data/firebase/FirebaseDataSource.kt
+++ app/src/main/java/com/stelianmorariu/antrics/data/firebase/FirebaseDataSource.kt
@@ -4,6 +4,8 @@
 
 package com.stelianmorariu.antrics.data.firebase
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.stelianmorariu.antrics.domain.model.MetricsProfile
 import io.reactivex.Single
@@ -12,36 +14,62 @@ import timber.log.Timber
 
 class FirebaseDataSource {
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
 
     fun getDeviceMetaData(deviceModel: String): Single<FirebaseDeviceMetadata> {
-        return Single.create<FirebaseDeviceMetadata> { emitter ->
-            val deviceMetaDataQuery = firestore
-                .collection(DEVICES_COLLECTION_NAME)
-                .whereEqualTo(DEVICE_MODEL_KEY, deviceModel)
+        return firebaseAnonymousAuth()
+            .flatMap {
+                Single.create<FirebaseDeviceMetadata> { emitter ->
+                    val deviceMetaDataQuery = firestore
+                        .collection(DEVICES_COLLECTION_NAME)
+                        .whereEqualTo(DEVICE_MODEL_KEY, deviceModel)
 
-            deviceMetaDataQuery.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                if (firebaseFirestoreException != null) {
-                    Timber.e(firebaseFirestoreException)
-                    emitter.onError(firebaseFirestoreException)
-                }
+                    deviceMetaDataQuery.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                        if (firebaseFirestoreException != null) {
+                            Timber.e(firebaseFirestoreException)
+                            emitter.onError(firebaseFirestoreException)
+                        }
 
-                querySnapshot?.let { snapshot ->
-                    if (snapshot.isEmpty) {
-                        emitter.onError(RuntimeException("Metadata not found for $deviceModel"))
-                    } else {
-                        val metadata = snapshot.toObjects(FirebaseDeviceMetadata::class.java)
-                        emitter.onSuccess(metadata.first())
+                        querySnapshot?.let { snapshot ->
+                            if (snapshot.isEmpty) {
+                                emitter.onError(RuntimeException("Metadata not found for $deviceModel"))
+                            } else {
+                                val metadata = snapshot.toObjects(FirebaseDeviceMetadata::class.java)
+                                emitter.onSuccess(metadata.first())
+                            }
+                        }
                     }
                 }
             }
-        }
-
     }
 
     fun saveDeviceMetrics(deviceMetricsPprofile: MetricsProfile) {
 
     }
+
+
+    /**
+     * Perform anonymous authentication to allow access to the Firebase data base.
+     */
+    private fun firebaseAnonymousAuth(): Single<FirebaseUser> {
+        return Single.create<FirebaseUser> { emitter ->
+            val currentUser = firebaseAuth.currentUser
+            if (currentUser != null) {
+                emitter.onSuccess(currentUser)
+            } else {
+                firebaseAuth.signInAnonymously()
+                    .addOnCompleteListener { authTask ->
+                        if (authTask.isSuccessful) {
+                            emitter.onSuccess(firebaseAuth.currentUser!!)
+                        } else {
+                            emitter.onError(RuntimeException("Can't perform anonymous sign in: ${authTask.exception?.localizedMessage}"))
+                        }
+                    }
+            }
+        }
+    }
+
 
     companion object {
         private const val DEVICES_COLLECTION_NAME = "devices"
