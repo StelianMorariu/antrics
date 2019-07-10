a2449e4f9de9fea7df540d31f14c0d0e39ef2cb1
stelian <stelian.morariu@gmail.com>
2019-07-10 12:57:30 +0100

    show popup menu from metrics profile

--- app/src/main/java/com/stelianmorariu/antrics/presentation/metrics/profile/MetricsProfileFragment.kt
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/metrics/profile/MetricsProfileFragment.kt
@@ -6,11 +6,10 @@ package com.stelianmorariu.antrics.presentation.metrics.profile
 
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.PopupMenu
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.MotionScene
 import androidx.core.content.ContextCompat
@@ -20,6 +19,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.stelianmorariu.antrics.R
@@ -31,12 +31,14 @@ import com.stelianmorariu.antrics.presentation.commons.loadDeviceImage
 import timber.log.Timber
 import javax.inject.Inject
 
-class MetricsProfileFragment : Fragment(), Injectable {
+class MetricsProfileFragment : Fragment(), Injectable, PopupMenu.OnMenuItemClickListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     lateinit var metricsViewModel: MetricsProfileViewModel
+
+    private lateinit var menuBtn: ImageView
 
     private lateinit var loadingImageView: ImageView
 
@@ -52,10 +54,12 @@ class MetricsProfileFragment : Fragment(), Injectable {
         val view = inflater.inflate(R.layout.fragment_metrics_profile, container, false)
 
         motionLayout = view.findViewById(R.id.motion_layout)
+        menuBtn = view.findViewById(R.id.btn_menu)
         loadingImageView = view.findViewById(R.id.device_image)
         titleTv = view.findViewById(R.id.title)
         recyclerView = view.findViewById(R.id.recyclerview)
 
+        setupMenu()
         setupRecyclerView()
         setupMotionLayout()
 
@@ -77,6 +81,24 @@ class MetricsProfileFragment : Fragment(), Injectable {
         return view
     }
 
+    /**
+     * This method will be invoked when a menu item is clicked if the item
+     * itself did not already handle the event.
+     *
+     * @param item the menu item that was clicked
+     * @return `true` if the event was handled, `false`
+     * otherwise
+     */
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_settings -> {
+                findNavController().navigate(R.id.action_metricsProfileFragment_to_settingsFragment)
+                true
+            }
+            else -> false
+        }
+    }
+
     private fun loadDeviceImage(statefulMetricsProfile: StatefulResource<MetricsProfile>) {
         val imageUrl = statefulMetricsProfile.data?.deviceImageUrl ?: ""
         if (imageUrl.isNullOrBlank()) {
@@ -86,6 +108,22 @@ class MetricsProfileFragment : Fragment(), Injectable {
         }
     }
 
+
+    private fun setupMenu() {
+        menuBtn.setOnClickListener { v ->
+            v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+            showPopMenu(v)
+        }
+    }
+
+    private fun showPopMenu(v: View) {
+        PopupMenu(context!!, v).apply {
+            // MainActivity implements OnMenuItemClickListener
+            setOnMenuItemClickListener(this@MetricsProfileFragment)
+            inflate(R.menu.main_menu)
+            show()
+        }
+    }
 
     private fun setupRecyclerView() {
         adapter = MetricsItemAdapter(context!!)
