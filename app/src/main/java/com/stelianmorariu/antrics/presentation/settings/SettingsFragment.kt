ce70611bb1d6e12c50e99a4bd07d703d802008b1
stelian <stelian.morariu@gmail.com>
2019-07-10 17:54:59 +0100

    change theme based on user preference

--- app/src/main/java/com/stelianmorariu/antrics/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/settings/SettingsFragment.kt
@@ -5,8 +5,10 @@
 package com.stelianmorariu.antrics.presentation.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.BuildCompat
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.stelianmorariu.antrics.R
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -22,7 +24,38 @@ class SettingsFragment : PreferenceFragmentCompat() {
      * [PreferenceScreen] with this key.
      */
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        setPreferencesFromResource(com.stelianmorariu.antrics.R.xml.preferences, rootKey)
+
+        val themePreference = findPreference<Preference>("theme")
+        themePreference?.setOnPreferenceChangeListener { pref, newValue ->
+            val themeOption = newValue as String
+            applyTheme(themeOption)
+            true
+        }
     }
 
+
+    private fun applyTheme(themePref: String) {
+        when (themePref) {
+            LIGHT_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            DARK_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                if (BuildCompat.isAtLeastQ()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
+        }
+    }
+
+    companion object {
+        val LIGHT_MODE = "light"
+        val DARK_MODE = "dark"
+        val DEFAULT_MODE = "default"
+    }
 }
