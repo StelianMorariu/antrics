517bebe3d717cfcc3b628bbc38a9b5c2342ad747
stelian <stelian.morariu@gmail.com>
2019-07-18 14:26:47 +0100

    extract code to change themes

---
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/commons/ThemeHelper.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+package com.stelianmorariu.antrics.presentation.commons
+
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.BuildCompat
+
+class ThemeHelper {
+
+    companion object {
+        val LIGHT_MODE = "light"
+        val DARK_MODE = "dark"
+        val DEFAULT_MODE = "default"
+
+        fun applyTheme(themePref: String) {
+            when (themePref) {
+                LIGHT_MODE -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                DARK_MODE -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                else -> {
+                    if (BuildCompat.isAtLeastQ()) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                    }
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/stelianmorariu/antrics/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/settings/SettingsFragment.kt
@@ -5,10 +5,9 @@
 package com.stelianmorariu.antrics.presentation.settings
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.BuildCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.stelianmorariu.antrics.presentation.commons.ThemeHelper
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -29,33 +28,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val themePreference = findPreference<Preference>("theme")
         themePreference?.setOnPreferenceChangeListener { pref, newValue ->
             val themeOption = newValue as String
-            applyTheme(themeOption)
+            ThemeHelper.applyTheme(themeOption)
             true
         }
     }
 
-
-    private fun applyTheme(themePref: String) {
-        when (themePref) {
-            LIGHT_MODE -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-            DARK_MODE -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }
-            else -> {
-                if (BuildCompat.isAtLeastQ()) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
-                }
-            }
-        }
-    }
-
-    companion object {
-        val LIGHT_MODE = "light"
-        val DARK_MODE = "dark"
-        val DEFAULT_MODE = "default"
-    }
 }
