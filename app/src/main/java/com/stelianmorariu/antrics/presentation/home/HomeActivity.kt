a7642f9af5abb37799446fa3e791a71f9819e89f
stelian <stelian.morariu@gmail.com>
2019-09-18 12:13:02 +0100

    draw behind the status bar

--- app/src/main/java/com/stelianmorariu/antrics/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/home/HomeActivity.kt
@@ -7,6 +7,8 @@ package com.stelianmorariu.antrics.presentation.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.stelianmorariu.antrics.R
 
@@ -21,6 +23,10 @@ class HomeActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.anim_no_translate, R.anim.anim_no_translate)
 
         setContentView(R.layout.activity_home)
+
+        val rootLayout: FrameLayout = findViewById(R.id.rootLayout)
+        rootLayout.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
     }
 
 
