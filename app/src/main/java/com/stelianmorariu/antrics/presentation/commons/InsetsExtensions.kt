061334ab0535860a0d3726e531c869a0f3a1a949
stelian <stelian.morariu@gmail.com>
2019-10-28 11:17:22 +0000

    add inset extension functions

---
+++ app/src/main/java/com/stelianmorariu/antrics/presentation/commons/InsetsExtensions.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright ©  2019 Stelian Morariu. All rights reserved.
+ */
+
+package com.stelianmorariu.antrics.presentation.commons
+
+import android.view.View
+import android.view.WindowInsets
+
+/**
+ * Extension functions and helpers to deal with applying window insets.
+ *
+ * [https://chris.banes.dev/2019/04/12/insets-listeners-to-layouts/]
+ */
+
+data class InitialPadding(
+    val left: Int, val top: Int,
+    val right: Int, val bottom: Int
+)
+
+fun View.doOnApplyWindowInsets(f: (View, WindowInsets, InitialPadding) -> Unit) {
+    // Create a snapshot of the view's padding state
+    val initialPadding = recordInitialPaddingForView(this)
+    // Set an actual OnApplyWindowInsetsListener which proxies to the given
+    // lambda, also passing in the original padding state
+    setOnApplyWindowInsetsListener { v, insets ->
+        f(v, insets, initialPadding)
+        // Always return the insets, so that children can also use them
+        insets
+    }
+    // request some insets
+    requestApplyInsetsWhenAttached()
+}
+
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        // We're already attached, just request as normal
+        requestApplyInsets()
+    } else {
+        // We're not attached to the hierarchy, add a listener to
+        // request when we are
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
+
+private fun recordInitialPaddingForView(view: View) = InitialPadding(
+    view.paddingLeft, view.paddingTop, view.paddingRight, view.paddingBottom
+)
